fcfc4311d228397bfd6e2bca3f778038777eea3c
LeeInGyu <developLee20@gmail.com>
2021-09-01 09:57:37 +0900

    2021-09-01 객체지향 1주차

---
+++ 객체지향프로그래밍/LSD.01/Prac2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    for(int i=0; i<3; i++){
+        cout << "3X";
+        cout << i+1;
+        cout << "=";
+        cout << 3*(i+1);
+        cout << "\n";
+    }
+}
