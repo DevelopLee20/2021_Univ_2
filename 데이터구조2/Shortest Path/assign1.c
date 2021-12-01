75f312bc5985e9b1d2b26bec868f51b5e886b609
LeeInGyu <developLee20@gmail.com>
2021-12-02 01:59:29 +0900

    데이터구조2 Shortest Path

--- 데이터구조2/Shortest Path/assign1.c
+++ 데이터구조2/Shortest Path/assign1.c
@@ -16,58 +16,47 @@ int distance[MAX_VERTICES];
 int found[MAX_VERTICES];
 
 int choose(int distance[], int n, int found[]){
-    int min, minpos;
+    int i, min, minpos;
     min = INT_MAX;
     minpos = -1;
 
-    for(int i=0; i<n; i++){
+    for(i=0; i<n; i++){
         if(distance[i] < min && !found[i]){
             min = distance[i];
             minpos = i;
         }
-        return minpos;
     }
+    return minpos;
 }
 
-void print_status(GraphType *g){ 
+void print_status(GraphType *g){
     static int step = 1;
-
-    printf("STEP %d: ", step++);
-    printf("distance: ");
-
     for(int i=0; i<g->n; i++){
         if(distance[i] == INF){
-            printf(" * ");
+            printf("INF "); // 수정된 부분
         }
         else{
-            printf("%2d ", distance[i]);
+            printf("%3d ", distance[i]);
         }
     }
     printf("\n");
-    printf(" found: ");
-
-    for(int i=0; i<g->n; i++){
-        printf("%2d ", found[i]);
-    }
-    printf("\n\n");
 }
 
-void shortest_path(GraphType *g, int start){
-    int u;
-
-    for(int i=0; i<g->n; i++){
+void short_path(GraphType *g, int start){
+    int i, u, w;
+    for(i=0; i<g->n; i++){
         distance[i] = g->weight[start][i];
         found[i] = FALSE;
     }
     found[start] = TRUE;
+    printf(" {%d}\n", start+1); // 수정된 부분
     distance[start] = 0;
-
-    for(int i=0; i<g->n-1; i++){
+    for(i=0; i<g->n-1; i++){
         print_status(g);
         u = choose(distance, g->n, found);
+        printf(" {%d}\n", u+1); // 수정된 부분
         found[u] = TRUE;
-
-        for(int w=0; w<g->n; w++){
+        for(w=0; w<g->n; w++){
             if(!found[w]){
                 if(distance[u] + g->weight[u][w] < distance[w]){
                     distance[w] = distance[u] + g->weight[u][w];
@@ -78,17 +67,21 @@ void shortest_path(GraphType *g, int start){
 }
 
 int main(void){
-    GraphType g = { 6, 
-    {
+    GraphType g = {6,{
         {0,10,INF,30,100,INF},
         {INF,0,50,INF,INF,INF},
         {INF,INF,0,INF,10,5},
         {INF,INF,20,0,INF,15},
         {INF,INF,INF,60,0,INF},
-        {INF,INF,INF,INF,INF,0}
-    }
-    };
-    shortest_path(&g, 0);
+        {INF,INF,INF,INF,INF,0},
+    }};
 
+    short_path(&g, 0);
+
+    int sum = 0;
+    for(int i=0; i<g.n; i++){
+        sum += distance[i];
+    }
+    printf("\nShortest Path: %d", sum);
     return 0;
 }
